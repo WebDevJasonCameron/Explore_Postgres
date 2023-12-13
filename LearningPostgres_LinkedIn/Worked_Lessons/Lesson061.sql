58fe40eb2258a99cb22de4df2c01538085e26d1e
Jason Cameron <dev.jasoncameron@gmail.com>
2023-12-13 06:10:29 -0600

    more concat

--- LearningPostgres_LinkedIn/Worked_Lessons/Lesson061.sql
+++ LearningPostgres_LinkedIn/Worked_Lessons/Lesson061.sql
@@ -1,1 +1,22 @@
--- Blank SQL file for Lesson 061
+-- When concatenation, how NULL values are handled
+-- ###############################################
+
+-- Using ||
+
+Select 'Hello' || NULL || 'World';
+
+-- Using Concat
+
+SELECT 
+	revenues_domestic,
+	revenues_international,
+	CONCAT(' | ', revenues_domestic, revenues_international) AS profits
+FROM movies_revenues
+;
+
+SELECT 
+	revenues_domestic,
+	revenues_international,
+	CONCAT_WS(' - ', revenues_domestic, revenues_international) AS profits
+FROM movies_revenues
+;
