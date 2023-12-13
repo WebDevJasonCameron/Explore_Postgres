60c25ee24add5243b5f3ddfbd1f2ed30d52bf21e
Jason Cameron <dev.jasoncameron@gmail.com>
2023-12-13 06:03:28 -0600

    concat

--- LearningPostgres_LinkedIn/Worked_Lessons/Lesson060.sql
+++ LearningPostgres_LinkedIn/Worked_Lessons/Lesson060.sql
@@ -1,1 +1,32 @@
--- Blank SQL file for Lesson 060
+-- Concatenate techniques
+-- ########################
+
+-- 1. Lets combine string 'hello' and 'world'
+
+SELECT 'hello' || ' ' || 'world';
+
+
+-- 2. Lets combinee actor first and last name as 'Actor Name'
+
+SELECT 
+	first_name || ' ' || last_name AS "Actor Name"
+FROM actors
+ORDER BY
+	first_name
+;
+
+-- 3. Lets print first name, last name and date ov birth of all actors seperated by comma
+
+SELECT 
+	CONCAT_WS(',', first_name, last_name, date_of_birth)
+FROM actors
+ORDER BY
+	first_name
+;
+
+
+
+
+
+
+
