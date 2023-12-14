80951b17305baf05028f6e8a64b90263553f5db9
Jason Cameron <dev.jasoncameron@gmail.com>
2023-12-14 06:06:38 -0600

    varchar

--- LearningPostgres_LinkedIn/Worked_Lessons/Lesson063.sql
+++ LearningPostgres_LinkedIn/Worked_Lessons/Lesson063.sql
@@ -1,1 +1,22 @@
--- Blank SQL file for Lesson 063
+-- Character Data Types
+-- ####################
+
+-- 1. Create table_encoding table
+
+SELECT CAST ('Jason' AS character(10)) as "Name";
+
+
+SELECT 'Jason'::char(10) as "Name";
+
+SELECT 
+	CAST ('Jason' AS character(10)) as "Name1",
+	'Jason'::char(10) as "Name"	
+;
+-- "Jason     "
+-- "Jason     "
+
+SELECT 'Jason'::varchar(10);
+-- "Jason"
+
+SELECT 'This is a test from the system'::varchar(10);
+-- "This is a "
