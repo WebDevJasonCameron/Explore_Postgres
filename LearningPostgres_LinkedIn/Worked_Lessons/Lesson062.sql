050488b2f494b0f89fb77cffefb1c2c367643383
Jason Cameron <dev.jasoncameron@gmail.com>
2023-12-14 05:43:54 -0600

    bools

--- LearningPostgres_LinkedIn/Worked_Lessons/Lesson062.sql
+++ LearningPostgres_LinkedIn/Worked_Lessons/Lesson062.sql
@@ -1,1 +1,19 @@
--- Blank SQL file for Lesson 062
+-- Boolean data type
+
+-- 1. Lets create a sample table called 'table_boolean'
+
+CREATE TABLE table_boolean(
+	product_id SERIAL PRIMARY KEY,
+	is_available BOOLEAN NOT NULL
+);
+
+-- 2. Lets insert some data
+
+INSERT INTO table_boolean(is_available)
+VALUES
+	(TRUE)
+;
+
+-- 3. Lets view the records
+
+SELECT * FROM table_boolean;
