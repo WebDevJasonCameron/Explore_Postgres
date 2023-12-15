369c9d03edfe8964b9ef044c095c49e28c01f5ca
Jason Cameron <dev.jasoncameron@gmail.com>
2023-12-15 05:18:54 -0600

    ints and serial working

--- LearningPostgres_LinkedIn/Worked_Lessons/Lesson063.sql
+++ LearningPostgres_LinkedIn/Worked_Lessons/Lesson063.sql
@@ -20,3 +20,21 @@ SELECT 'Jason'::varchar(10);
 
 SELECT 'This is a test from the system'::varchar(10);
 -- "This is a "
+
+SELECT 'ABCD 123'::varchar(10);
+-- "ABCD 123"
+
+
+CREATE TABLE table_characters(
+	col_char CHAR(10), 
+	col_varchar VARCHAR(10), 
+	col_text TEXT
+);
+
+SELECT * FROM table_characters;
+
+INSERT INTO table_characters (col_char, col_varchar, col_text)
+VALUES
+	('ABC', 'ABC', 'ABC'),
+	('xyz', 'xyz', 'xyz')
+;
--- LearningPostgres_LinkedIn/Worked_Lessons/Lesson064.sql
+++ LearningPostgres_LinkedIn/Worked_Lessons/Lesson064.sql
@@ -1,1 +1,24 @@
--- Blank SQL file for Lesson 064
+-- Numbers Data types
+-- ####################
+
+-- Lets create a table with SERIAL data type
+
+CREATE TABLE table_serial(
+	product_id SERIAL,
+	product_name VARCHAR(100)
+);
+
+-- Lets insert some data
+
+INSERT INTO table_serial (product_name)
+VALUES
+	('pen'),
+	('pencil')
+;
+
+SELECT * FROM table_serial;
+
+INSERT INTO table_serial (product_name)
+VALUES
+	('pencil2')
+;
