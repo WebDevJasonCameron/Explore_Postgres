48100cfe315019922bc2a037297b9d7e569aea32
Jason Cameron <dev.jasoncameron@gmail.com>
2023-12-16 09:57:44 -0600

    hstor is amazing!

--- LearningPostgres_LinkedIn/Worked_Lessons/Lesson072.sql
+++ LearningPostgres_LinkedIn/Worked_Lessons/Lesson072.sql
@@ -1,1 +1,53 @@
--- Blank SQL file for Lesson 072
+-- Array data types
+-- ####################
+
+/*
+	1. Every data type has its own companion array type e.g.,
+	
+		- integer has an integer[] array type,
+		- character has character[] array type
+		
+	2. An array data type is named by appending square brackets ([])
+	   to the data type name of the array elements
+	   
+		variable []
+		
+		phones TEXT []
+		
+		class_name VARCHAR][]
+*/
+
+-- 1. Create sample table
+CREATE TABLE table_array (
+	id SERIAL,
+	name VARCHAR(100),
+	phones TEXT [] 					-- <- OUR ARRAY
+);
+
+
+-- 2. Insert data
+INSERT INTO table_array 
+	(name, phones)
+VALUES
+	('Adam', ARRAY ['(800)123-4567', '(888)123-4567']);
+
+INSERT INTO table_array
+	(name, phones)
+VALUES
+	('LINDA', ARRAY ['(206)123-4567', '(214)123-4567']);
+
+-- 3. Query data
+SELECT * FROM table_array;
+
+SELECT 
+	name,
+	phones [1]
+FROM 
+	table_array;
+	
+SELECT * 
+FROM table_array
+WHERE
+	phones[2] = '(214)123-4567';
+
+
--- LearningPostgres_LinkedIn/Worked_Lessons/Lesson073.sql
+++ LearningPostgres_LinkedIn/Worked_Lessons/Lesson073.sql
@@ -1,1 +1,48 @@
--- Blank SQL file for Lesson 073
+-- hstore data type
+-- ##################
+
+/*
+	1. hstore is a data type that store data into key-value pairs
+	2. The hstore module implements the hstore data type
+	3. The keys and values are just text strings only
+*/
+
+-- 1. Inall hstore extensions first
+CREATE EXTENSION IF NOT EXISTS hstore;
+
+-- 2. Create a sample table
+CREATE TABLE table_hstore (
+	book_id SERIAL PRIMARY KEY,
+	title VARCHAR(100) NOT NULL,
+	book_info hstore
+);
+
+-- 3. Add data
+INSERT INTO table_hstore 
+	(title, book_info)
+VALUES
+	('TITLE 1',
+	'
+	 	"publisher" => "ABC publisher",
+	 	"paper_cost" => "10.00",
+	 	"e_cost" => "5.85"
+	 ')
+;
+
+INSERT INTO table_hstore 
+	(title, book_info)
+VALUES
+	('TITLE 2',
+	'
+	 	"publisher" => "ABC publisher2",
+	 	"paper_cost" => "20.00",
+	 	"e_cost" => "7.85"
+	 ')
+;
+
+-- 4. Query data
+SELECT * FROM table_hstore;
+
+-- -> operator
+SELECT book_info -> 'publisher'
+FROM table_hstore;
