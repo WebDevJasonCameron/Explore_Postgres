363c6ed64b984c60303516378b56b0ee482a5311
Jason Cameron <dev.jasoncameron@gmail.com>
2023-12-20 05:31:03 -0600

    domain for email

--- LearningPostgres_LinkedIn/Worked_Lessons/Lesson090.sql
+++ LearningPostgres_LinkedIn/Worked_Lessons/Lesson090.sql
@@ -1,1 +1,25 @@
--- Blank SQL file for Lesson 090
+-- 6: 'proper_email' domain 
+
+CREATE DOMAIN propper_email VARCHAR(150)
+CHECK (VALUE ~* '^[A-Za-z0-9._%~]+@[A-Za-z0-9.-]+[.][A-Za-z]+$');
+
+DROP DOMAIN propper_email;
+DROP TABLE clients_names;
+
+CREATE TABLE clients_names(
+	client_name_id SERIAL PRIMARY KEY,
+	email propper_email
+);
+
+SELECT * FROM clients_names;
+
+INSERT INTO clients_names 
+	(email)
+VALUES
+	('smash@gmail.com');
+	
+	
+	
+	
+	
+	
