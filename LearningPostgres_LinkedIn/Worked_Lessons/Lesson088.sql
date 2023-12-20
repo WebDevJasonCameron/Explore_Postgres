f9d4f2ff99ee4900cb0fa18252284b1feac6edfe
Jason Cameron <dev.jasoncameron@gmail.com>
2023-12-19 18:35:18 -0600

    more on domain creations

--- LearningPostgres_LinkedIn/Worked_Lessons/Lesson088.sql
+++ LearningPostgres_LinkedIn/Worked_Lessons/Lesson088.sql
@@ -1,1 +1,15 @@
--- Blank SQL file for Lesson 088
+-- Example 4: 'positive_numeric' domain with a position NUMERIC I.E. > 0
+
+CREATE DOMAIN positive_numeric INT NOT NULL CHECK (VALUE > 0);
+
+CREATE TABLE sample (	
+	sample_id SERIAL,
+	value_num positive_numeric
+);
+
+INSERT INTO sample
+	(value_num)
+VALUES
+	(10);
+	
+SELECT * FROM sample;
--- LearningPostgres_LinkedIn/Worked_Lessons/Lesson089.sql
+++ LearningPostgres_LinkedIn/Worked_Lessons/Lesson089.sql
@@ -1,1 +1,19 @@
--- Blank SQL file for Lesson 089
+-- example 5: 'us_postal_code' domain to check for valid us postal code format
+
+CREATE DOMAIN us_postal_code AS TEXT
+CHECK(
+	VALUE ~'^\d{5}$'
+	OR VALUE ~'^\D{5}~\d{4}$'
+);
+
+CREATE TABLE addresses (
+	address_id SERIAL PRIMARY KEY,
+	postal_code us_postal_code
+);
+
+INSERT INTO addresses 
+	(postal_code)
+VALUES
+	('10000');
+
+SELECT * FROM addresses;
